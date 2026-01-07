5e200e49fd77f25a3fee1d88b91834fcf68b4694
razmit <rolin.azmitia.iii@gmail.com>
2026-01-06 21:56:44 -0600

    update(Mass_site_creation.ps1): Completed the logic to allow it to create subsites from the Excel file. Reporting features are pending.

--- Windows/RSM-related/PnP/Mass_site_creation.ps1
+++ Windows/RSM-related/PnP/Mass_site_creation.ps1
@@ -6,7 +6,69 @@
 # Date: January 2026
 # ============================================================================
 
-Connect-PnPOnline -Url https://rsmnet.sharepoint.com/sites/IWS_ORM_PCAOB_Inspections -clientId f6666fe0-04e6-419a-b4bb-4025060af8f5 -interactive
+# Get the Title from the source subsite to replicate it in the new subsite
+function Get-SourceSubSiteInfo {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $SourceSiteUrl
+    )
+    
+    try {
+        Connect-PnPOnline -Url $SourceSiteUrl -clientId f6666fe0-04e6-419a-b4bb-4025060af8f5 -interactive
+        
+        Write-Host "Connected to source subsite: $SourceSiteUrl" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Error connecting to source subsite: $SourceSiteUrl" -ForegroundColor Red
+        exit
+    }
+    
+    try {
+        # Get just the title of the source subsite
+        $web = Get-PnPWeb -ErrorAction Stop
+        $title = $web.Title
+        return $title
+    }
+    catch {
+        Write-Host "Error retrieving info for source subsite: $SourceSiteUrl" -ForegroundColor Red
+        return $false
+    }
+    
+}
+
+
+function New-SubSite {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string[]]
+        $SubSiteUrls,
+        [Parameter(Mandatory=$true)]
+        [string[]]
+        $SubSiteTitles
+    )
+    
+    $newParentSiteUrl = "https://rsmnet.sharepoint.com/sites/IWS_ORM_PCAOB_Inspections"
+    
+    Connect-PnPOnline -Url $newParentSiteUrl -clientId f6666fe0-04e6-419a-b4bb-4025060af8f5 -interactive
+    
+    $i = 0
+    # Create the new subsite
+    try {
+        foreach ($SubSiteUrl in $SubSiteUrls) {
+            # Write-Host "Creating new subsite at URL: $newParentSiteUrl/$SubSiteUrl with title $($SubSiteTitles[$i])" -ForegroundColor Cyan
+            
+            New-PnPWeb -Title $SubSiteTitles[$i] -Url $SubSiteUrl -Template "STS#3" -Locale 1033 -ErrorAction SilentlyContinue
+            Write-Host "+++ Successfully created new subsite: $newParentSiteUrl/$SubSiteUrl with title $($SubSiteTitles[$i])" -ForegroundColor Green
+            Write-Host "-----------------------------------------" -ForegroundColor White
+            $i++
+        }
+    }
+    catch {
+        Write-Host "XXX Failed creating the new subsite." -ForegroundColor Red
+        continue
+    }
+}
 
 # We load the file containing the sites to be created
 $pathToFile = "C:\Users\E095713\Downloads\PCAOB_Mapping.xlsx"
@@ -14,37 +76,84 @@ $pathToFile = "C:\Users\E095713\Downloads\PCAOB_Mapping.xlsx"
 # Get the data from the Excel file
 $excelData = Import-Excel -Path $pathToFile -WorksheetName "Sheet1"
 
-$excelData | Select-Object 'Source', 'New URL of child site' -First 5 | Format-Table -AutoSize
+# $excelData | Select-Object 'Source', 'New URL of child site' -First 10 | Format-Table -AutoSize
 
-# Example: Accessing specific cell values for the source site
-$targetRow = 0
+# Name of column where the source site URLs are stored
 $sourceTargetColumn = "Source"
 
-# Example: Accessing specific cell values for the new site URL
+# Name of column where the potential new site URLs are stored
 $newSiteTargetColumn = "New URL of child site"
 
-# Get the source site URL
-$sourceCellValue = $excelData[$targetRow].$sourceTargetColumn
+# Array to save all of the URLs for the new subsites
+$newSubSiteUrls = @()
 
-# Check if there's a value for the new site URL
-$newSiteCellValue = $excelData[$targetRow].$newSiteTargetColumn
+# Array to save all of the titles for the new subsites
+$newSubSiteTitles = @()
 
-if ($null -eq $sourceCellValue) {
-    Write-Host "Cell at Source row $targetRow, column '$sourceTargetColumn' is null or does not exist." -ForegroundColor Red
-}
-else {
-    Write-Host "Value at row $targetRow, column '$sourceTargetColumn': $sourceCellValue" -ForegroundColor Green
+$definedSubSitesCounter = 0
+
+foreach ($row in $excelData) {
+    $sourceSite = $row.$sourceTargetColumn
+    $newSiteURL = $row.$newSiteTargetColumn
     
-    if ($null -eq $newSiteCellValue) {
-        Write-Host "Cell at New URL of child site row $targetRow, column '$newSiteTargetColumn' is also null or does not exist." -ForegroundColor Yellow
-    }
-    else {
-        Write-Host "Value at row $targetRow, column '$newSiteTargetColumn': $newSiteCellValue" -ForegroundColor Cyan
+    if ($null -ne $newSiteURL) {
+        # If there's an already set subsite URL
+        $predefinidedNewSiteURL = $newSiteURL -split "/"
+        $newSiteName = $predefinidedNewSiteURL[2]
+        
+        $returnedSourceSiteInfo = Get-SourceSubSiteInfo -SourceSiteUrl $sourceSite
+        
+        # Right now, the only subsite URL that is returning an error is the "PCAOBInspections/000" one as it appears to not exist anymore
+        if (!$returnedSourceSiteInfo) {
+            Write-Host "Skipping creation of new subsite due to error in retrieving source site info." -ForegroundColor Red
+            continue
+        }
+        
+        # Add the subsite URL to the array
+        $newSubSiteUrls += $newSiteName
+        
+        # Add the title to the array
+        $newSubSiteTitles += $returnedSourceSiteInfo
+        
+        $definedSubSitesCounter++
+    } else {
+        # If there's no predefined subsite URL, we can use the last segment of the source site URL
+        $anchorSegment = "PCAOBInspections"
+        
+        # Split the URL by "/" and find everything after the anchor
+        $urlParts = $sourceSite.TrimEnd('/') -split '/'
+        $anchorIndex = $urlParts.IndexOf($anchorSegment)
+
+        # Get all segments AFTER the anchor
+        $segmentsAfterAnchor = $urlParts[($anchorIndex + 1)..($urlParts.Count - 1)]
+        
+        if ($segmentsAfterAnchor.Count -eq 0) {
+            Write-Host "No segments found after anchor '$anchorSegment' in URL: $sourceSite. Skipping." -ForegroundColor Red
+            continue
+        } elseif ($segmentsAfterAnchor.Count -ge 2) {
+            Write-Host "Multiple segments found after anchor '$anchorSegment': $($segmentsAfterAnchor -join '/'). Using only the first segment: $($segmentsAfterAnchor[0])" -ForegroundColor Yellow
+            $newSiteName = $segmentsAfterAnchor[0]
+        } else {
+            $newSiteName = $segmentsAfterAnchor[0]
+        }
+        
+        $returnedSourceSiteInfo = Get-SourceSubSiteInfo -SourceSiteUrl $sourceSite
+        
+        if (!$returnedSourceSiteInfo) {
+            Write-Host "Skipping creation of new subsite due to error in retrieving source site info." -ForegroundColor Red
+            continue
+        }   
+        
+        # Add the subsite URL to the array
+        $newSubSiteUrls += $newSiteName
+        
+        # Add the title to the array
+        $newSubSiteTitles += $returnedSourceSiteInfo
     }
 }
 
+# Create the new subsites
+New-SubSite -SubSiteUrls $newSubSiteUrls -SubSiteTitle $newSubSiteTitles
 
-# # Subsite naming for the 12- series
-# for ($i = 0; $i -lt $array.Count; $i++) {
-#     <# Action that will repeat until the condition is met #>
-# }
+
+Write-Host "Total number of defined subsites: $definedSubSitesCounter" -ForegroundColor Yellow
